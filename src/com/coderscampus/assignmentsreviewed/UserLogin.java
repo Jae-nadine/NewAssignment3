d4a808f4d841139e4427b7ba5932ac0997d79fd4
Jae-Nadine <jae.nadine@icloud.com>
2024-10-13 03:41:54 -0700

    Initial Commit
    Created a new Assignment3 - classes were nested in one class in old assignment. Created 3 classes respectively and data.txt file

---
+++ src/com/coderscampus/assignmentsreviewed/UserLogin.java
@@ -0,0 +1,5 @@
+package com.coderscampus.assignmentsreviewed;
+
+public class UserLogin {
+
+}
---
+++ src/com/coderscampus/assignmentsreviewed/UserLoginApplication.java
@@ -0,0 +1,9 @@
+package com.coderscampus.assignmentsreviewed;
+
+public class UserLoginApplication {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
